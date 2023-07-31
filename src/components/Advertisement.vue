c112ae835f3c8e39972a44f7d2f402906d1cf888
ismailkhaan <ismailkhan358391@gmail.com>
2023-07-31 07:17:17 +0500

    fixed addv

--- src/components/Advertisement.vue
+++ src/components/Advertisement.vue
@@ -1,29 +1,26 @@
 <script setup>
 import { ref } from 'vue';
-
+import promotion from '/images/promotion.png'
 const isPromo = ref(true)
 </script>
 
 <template>
   <div v-if="isPromo" class="advertisement">
     <div @click="isPromo = !isPromo" class="cross"> &#9587</div>
+    <img class="promo" :src="promotion" alt="promo">
     <div class="trife">立即下載</div>
   </div>
 </template>
 
 <style scoped>
 .advertisement {
-  transition: all ease-out .3s;
-background: url('/images/promotion.png') no-repeat;
-  background-size: 100% 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
   background-color: whitesmoke;
-  width: 957;
-  overflow: hidden;
   height: 60px;
   padding: 0 14px;
+  overflow: hidden;
 }
 
 .trife {
@@ -32,6 +29,11 @@ background: url('/images/promotion.png') no-repeat;
   background: linear-gradient(180deg,#feb68e,#f76f3f);
   color: white;
   font-size: 12px;
+}
+img{
+  width: 70%;
+  height: 70px;
+  object-fit: fill;
 }
 .cross{
   font-size: 30px;
